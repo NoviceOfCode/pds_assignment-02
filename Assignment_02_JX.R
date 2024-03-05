521b795a1cdc203e0ba8f0e6003b6fbaaae8e0bc
Jacob Xayaphet <jxcdx@umsystem.edu>
2024-03-05 02:08:57 -0600

    Added R Code

--- Assignment_02_JX.R
+++ Assignment_02_JX.R
@@ -1,6 +1,8 @@
 install.packages("dplyr")
 install.package("stringr")
 library(dplyr)
+install.package("readr")
+library(readr)
 train <- read_csv("train.csv")
 
 #Finding counts for missing values
@@ -34,3 +36,27 @@ adjusted_power <- adjusted_engine %>%
   mutate(Power = gsub(" bhp", "", Power)) # Removing bhp
 final_adjusted <- adjusted_power %>%
   mutate(New_Price = gsub(" Lakh", "", New_Price)) #Removing lakh
+
+#Question C
+library(tidyr)
+#One hot encoding for Fuel Types
+unique_fuel_types <- unique(train$Fuel_Type)
+fuel_type_one_hot <- train %>%
+  mutate(Petrol = ifelse(Fuel_Type == "Petrol", 1, 0),
+         Diesel = ifelse(Fuel_Type == "Diesel", 1, 0),
+         Electric = ifelse(Fuel_Type == "Electric", 1, 0))
+final_fuel_type_one_hot <- fuel_type_one_hot %>%
+  select(-Fuel_Type)
+
+# One Hot Recording for Transmission
+unique_transmission <- unique(train$Transmission)
+transmission_one_hot_encoded <-final_fuel_type_one_hot %>%
+  mutate(Manual = ifelse(Transmission == "Petrol", 1, 0),
+                Diesel = ifelse(Transmission == "Diesel", 1, 0))
+final_transmission <- transmission_one_hot_encoded %>%
+  select(-Transmission)
+
+#Question D adding a feature
+current_year_car_train <- train %>%
+  mutate( Car_Age = 2024 -Year)
+
